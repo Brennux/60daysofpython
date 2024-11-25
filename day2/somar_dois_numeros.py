fb318e308b893b09b42663ac57246d633ed15690
Brennux <nascimentobreno730@gmail.com>
2024-11-25 16:54:08 -0300

    dia 2 de programação em python

---
+++ day2/somar_dois_numeros.py
@@ -0,0 +1,4 @@
+n1 = int(input("digite um numero:"))
+n2 = int(input("digite outro:"))
+soma = n1 + n2
+print(f'A soma entre {n1} e {n2} é {soma}!')
---
+++ day2/somar_dois_numeros_2.py
@@ -0,0 +1,14 @@
+def somar(numero1, numero2):
+    somar_dois_numeros = numero1 + numero2
+    print("somar_dois_numeros")
+    return somar_dois_numeros
+   # somar(5,5)
+   
+def somar_dois_num():
+       numero1 = int(input('digite um numero:'))
+       numero2 = int(input('digite outro:'))
+       somar_dois_numeros = numero1 + numero2
+       print(somar_dois_numeros)
+       return somar_dois_numeros
+   
+somar_dois_num()
