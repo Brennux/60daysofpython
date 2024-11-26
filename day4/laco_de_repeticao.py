61ca4e8c5483e7a498db07978193577831d1b582
Brennux <nascimentobreno730@gmail.com>
2024-11-25 21:01:30 -0300

    dia 4 adicionadno laco de repetição

---
+++ day4/laco_de_repeticao.py
@@ -0,0 +1,10 @@
+# usando o for para imprimir numeros de 0 a 10
+for numero in range(21):
+    print(numero)
+    
+#usando o while para imprimir numeros
+
+numero = 5
+while numero < 21:
+    print(numero)
+    numero += 1
